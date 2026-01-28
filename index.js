38cf62f9e828416e3ea25ea24c3330374c1ac62d
Tobias <tobiaslopez353@gmail.com>
2026-01-28 09:45:13 -0300

    inicializar sincronización de BD al inicio del servidor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express"); //exportamos dependencias
-
+const sequelize = require("./data/db");
 const topicsRoutes = require("./routes/topics.routes");
+require("./models/topic.model");
 
 const app = express();
 
@@ -15,7 +16,15 @@ app.set("view engine", "ejs");
 // ruta
 app.use("/topics", topicsRoutes)
 
-// levantamos el servidor
-app.listen( 3000, () => { 
-    console.log("Servidor escuchando en: http://localhost:3000/topics"); } );
+// levantamos el servidor con la bd
+sequelize.sync({ force: false }) // force: false no borra tus datos al reiniciar
+    .then(() => {
+        console.log(" Base de datos sincronizada");
+        app.listen(3000, () => { 
+            console.log(" Servidor escuchando en: http://localhost:3000/topics"); 
+        });
+    })
+    .catch(error => {
+        console.error(" Error al conectar DB:", error);
+    });
 
