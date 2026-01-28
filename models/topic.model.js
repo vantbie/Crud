1e33270a25c92a4124f21d51e4aeb5ef9be7e373
Tobias <tobiaslopez353@gmail.com>
2026-01-28 09:41:28 -0300

    Definicion del modelo Topic

--- models/topic.model.js
+++ models/topic.model.js
@@ -1,8 +1,29 @@
-function createTopic(title) {
+const { DataTypes } = require('sequelize');
+const sequelize = require('../data/db'); 
+
+//definimos el modelo "Topic"
+const Topic = sequelize.define("Topic", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  votes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0 
+    }
+},{
+  // CONFIGURACIÓN CLAVE:
+  timestamps: false, // Le dice a Sequelize: "No busques createdAt ni updatedAt"
+  tableName: 'topics' // Asegura el nombre de la tabla
+});
+
+module.exports = Topic;
+
+/*function createTopic(title) {
   return {
     id: Date.now(),
     title,
     votes: 0,
-    links: []
+    //links: []
   };
-}
+}*/
